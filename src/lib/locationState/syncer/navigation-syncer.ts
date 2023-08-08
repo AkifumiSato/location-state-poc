c86ec3c49eda536f2d714a849908ae90b530688d
佐藤昭文 <akfm.sato@gmail.com>
2023-08-08 22:42:05 +0900

    Syncer's navigation to optional

--- src/lib/locationState/syncer/navigation-syncer.ts
+++ src/lib/locationState/syncer/navigation-syncer.ts
@@ -3,10 +3,10 @@ import { Syncer } from "@/lib/locationState/syncer/types";
 export class NavigationSyncer implements Syncer {
   private lastPublishedEntryKey?: string;
 
-  constructor(private readonly navigation: Navigation) {}
+  constructor(private readonly navigation?: Navigation) {}
 
   key(): string | undefined {
-    return this.navigation.currentEntry?.key;
+    return this.navigation?.currentEntry?.key;
   }
 
   sync({
@@ -16,10 +16,10 @@ export class NavigationSyncer implements Syncer {
     listener: (key: string) => void;
     signal: AbortSignal;
   }): void {
-    this.navigation.addEventListener(
+    this.navigation?.addEventListener(
       "currententrychange",
       (e) => {
-        if (this.lastPublishedEntryKey === this.navigation.currentEntry?.key) {
+        if (this.lastPublishedEntryKey === this.navigation?.currentEntry?.key) {
           // `history.replace` may cause events to fire with the same key.
           // https://github.com/WICG/navigation-api#the-current-entry
           return;
@@ -29,8 +29,8 @@ export class NavigationSyncer implements Syncer {
           (e as NavigateEvent).navigationType === "push" ||
           (e as NavigateEvent).navigationType === "replace"
         ) {
-          listener(this.navigation.currentEntry?.key as string);
-          this.lastPublishedEntryKey = this.navigation.currentEntry?.key;
+          listener(this.navigation?.currentEntry?.key!);
+          this.lastPublishedEntryKey = this.navigation?.currentEntry?.key;
         }
       },
       {
