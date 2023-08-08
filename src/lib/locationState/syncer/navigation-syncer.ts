c99221dac2c205deca50b8c2eafb326f2684db7e
佐藤昭文 <akfm.sato@gmail.com>
2023-08-08 22:16:50 +0900

    skip same entry key published

--- src/lib/locationState/syncer/navigation-syncer.ts
+++ src/lib/locationState/syncer/navigation-syncer.ts
@@ -1,6 +1,8 @@
 import { Syncer } from "@/lib/locationState/syncer/types";
 
 export class NavigationSyncer implements Syncer {
+  private lastPublishedEntryKey?: string;
+
   constructor(private readonly navigation: Navigation) {}
 
   key(): string | undefined {
@@ -17,8 +19,15 @@ export class NavigationSyncer implements Syncer {
     this.navigation.addEventListener(
       "currententrychange",
       (e) => {
+        if (this.lastPublishedEntryKey === this.navigation.currentEntry?.key) {
+          // `history.replace` may cause events to fire with the same key.
+          // https://github.com/WICG/navigation-api#the-current-entry
+          return;
+        }
+
         if (e.navigationType === "push" || e.navigationType === "replace") {
           listener(this.navigation.currentEntry?.key as string);
+          this.lastPublishedEntryKey = this.navigation.currentEntry?.key;
         }
       },
       {
